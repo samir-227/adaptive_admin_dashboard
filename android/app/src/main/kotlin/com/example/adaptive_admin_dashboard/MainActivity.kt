0bbf4eb68559704633b39c958bb06c97296d6398
samir-227 <samermedhat227@gmail.com>
2025-01-01 20:29:00 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/adaptive_admin_dashboard/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.adaptive_admin_dashboard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AdaptiveAdminDashBoard());
+}
+
+class AdaptiveAdminDashBoard extends StatelessWidget {
+  const AdaptiveAdminDashBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
