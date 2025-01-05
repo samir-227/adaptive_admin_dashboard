df1a3901087cf2167e5dfbc707653761a2b85c11
samir-227 <samermedhat227@gmail.com>
2025-01-05 14:26:27 +0200

    custom drawer item done

--- lib/views/widgets/custom_drawer.dart
+++ lib/views/widgets/custom_drawer.dart
@@ -1,5 +1,7 @@
+import 'package:adaptive_admin_dashboard/core/models/drawer_item_model.dart';
 import 'package:adaptive_admin_dashboard/core/models/user_info_model.dart';
 import 'package:adaptive_admin_dashboard/core/utls/app_images.dart';
+import 'package:adaptive_admin_dashboard/views/widgets/active_and_inactive_item.dart';
 import 'package:adaptive_admin_dashboard/views/widgets/drewer_list_view.dart';
 import 'package:adaptive_admin_dashboard/views/widgets/user_info_listTile.dart';
 import 'package:flutter/material.dart';
@@ -30,6 +32,30 @@ class CustomDrawer extends StatelessWidget {
             ),
           ),
           const DrawerListView(),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                const Expanded(
+                  child: SizedBox(
+                    height: 20,
+                  ),
+                ),
+                InActiveDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                      title: 'Setting system',
+                      image: ImagesAssets.imageSettings),
+                ),
+                InActiveDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                      title: 'Logout account', image: ImagesAssets.imageLogout),
+                ),
+                const SizedBox(
+                  height: 48,
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
