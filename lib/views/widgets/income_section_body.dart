108032027897d5b75a88b72d0363a55f39e5b0c1
samir-227 <samermedhat227@gmail.com>
2025-01-24 22:29:59 +0200

    income section body widget done

--- lib/views/widgets/income_section_body.dart
+++ lib/views/widgets/income_section_body.dart
@@ -1,3 +1,4 @@
+import 'package:adaptive_admin_dashboard/views/widgets/detailed_income_chart.dart';
 import 'package:adaptive_admin_dashboard/views/widgets/income_chart.dart';
 import 'package:adaptive_admin_dashboard/views/widgets/income_details.dart';
 import 'package:flutter/material.dart';
@@ -11,7 +12,11 @@ class IncomeSectionBody extends StatelessWidget {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     return width >= 1200 && width < 1674
-        ? SizedBox()
+        ? const Expanded(
+            child: Padding(
+            padding: EdgeInsets.all(16),
+            child: DetailedIncomeChart(),
+          ))
         : const Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
